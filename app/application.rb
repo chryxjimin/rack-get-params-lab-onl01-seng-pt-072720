88a1dc45be569b288570112e803389b7431250f4
Christie Yoon <cleex1999@gmail.com>
2020-10-09 15:30:22 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -19,8 +19,11 @@ class Application
         resp.write "#{item}\n"
       end
     end
-  elsif req.path.match(/add/)
-
+    elsif req.path.match(/add/)
+        search_term = req.params["q"]
+        if @@items.include?(search_term)
+           @@cart << search_term
+           resp.write "added #{search_term}"
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
